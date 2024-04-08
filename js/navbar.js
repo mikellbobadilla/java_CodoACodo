e693fcb912d78e4176d894b4a90dcfd7c7b8cc8b
sting <bobadillasting@gmail.com>
2024-04-08 19:08:12 -0300

    Refactor navbar.js and estilos.css

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-
-const btnToggle = document.getElementById('btn-toggle')
-const navItems = document.getElementById('nav')
-
-btnToggle.addEventListener('click', () => {
-    navItems.classList.toggle('visible')
-})
-
-
